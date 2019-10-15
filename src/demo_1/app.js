6516237f8cc9b631353d625d158374669de64662
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-10-15 18:57:07 +0900

    hjkim showcase

--- src/demo_1/app.js
+++ src/demo_1/app.js
@@ -15,7 +15,6 @@ app.listen(3000);
 // step 2. instance obejct for mutler.
 // step 3. monitor post method using app.post, running uploader front to backend if a user requests to you web.
 const multer = require('multer');
-var spawn = require('child_process');
 
 const storage = multer.diskStorage({
 	destination: function(req, file, callback){
@@ -29,8 +28,9 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage});
 app.post('/upload_form', upload.single('fileUpload'), (req, res) => {
 	//res.send('success');
+	const { spawn } = require('child_process');
 	const cmd = spawn('/usr/bin/python3', ['./test.py'])
-	ls.stdout.on('data', (data) =>{
+	cmd.stdout.on('data', (data) =>{
 		console.log("succ");
 	})
 });
