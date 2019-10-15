11caa48b1f5a911463bd0bfd721ce323e8ae7d22
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-10-15 15:05:19 +0900

    hjkim showcase

--- src/demo_1/app.js
+++ src/demo_1/app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
 // viewed at http://localhost:8080
+
 app.get('/', function(req, res) {
     res.sendFile(path.join("/home/ubuntu/StarAdmin-Free-Bootstrap-Admin-Template/src/demo_1" + '/index.html'));
 });
 app.use(express.static("/home/ubuntu/StarAdmin-Free-Bootstrap-Admin-Template/src/demo_1/."));
 app.use("/assets", express.static("/home/ubuntu/StarAdmin-Free-Bootstrap-Admin-Template/src/assets"));
 app.listen(3000);
+
+// file uploader using multer
+// Step 1. npm install multer
+// step 2. instance obejct for mutler.
+// step 3. monitor post method using app.post, running uploader front to backend if a user requests to you web.
+var multer = require('multer');
+const upload = multer({dest: '/assets/uploaded'});
+app.post('/upload', upload.single('img'), (req, res) => {
+	console.log(req.file);
+})
+
