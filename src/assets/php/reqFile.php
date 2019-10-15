0f6fd27a459e5f8f3e2ae4f26db3ee38cdeafa05
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-10-15 17:53:15 +0900

    hjkim showcase

---
+++ src/assets/php/reqFile.php
@@ -0,0 +1,37 @@
+<?php
+// 설정
+$uploads_dir = './uploaded';
+$allowed_ext = array('jpg','jpeg','png','gif');
+ 
+// 변수 정리
+$error = $_FILES['imageFile']['error'];
+$name = $_FILES['imageFile']['name'];
+$ext = array_pop(explode('.', $name));
+ 
+// 오류 확인
+if( $error != UPLOAD_ERR_OK ) {
+	switch( $error ) {
+		case UPLOAD_ERR_INI_SIZE:
+		case UPLOAD_ERR_FORM_SIZE:
+			echo "파일이 너무 큽니다. ($error)";
+			break;
+		case UPLOAD_ERR_NO_FILE:
+			echo "파일이 첨부되지 않았습니다. ($error)";
+			break;
+		default:
+			echo "파일이 제대로 업로드되지 않았습니다. ($error)";
+	}
+	exit;
+}
+ 
+// 확장자 확인
+if( !in_array($ext, $allowed_ext) ) {
+	echo "허용되지 않는 확장자입니다.";
+	exit;
+}
+ 
+// 파일 이동
+//move_uploaded_file( $_FILES['imageFile']['tmp_name'], "$uploads_dir/$name");
+
+// 파일 정보 출력
+echo "Uploaded file success"
--- src/demo_1/app.js
+++ src/demo_1/app.js
@@ -9,28 +9,3 @@ app.get('/', function(req, res) {
 app.use(express.static("/home/ubuntu/StarAdmin-Free-Bootstrap-Admin-Template/src/demo_1/."));
 app.use("/assets", express.static("/home/ubuntu/StarAdmin-Free-Bootstrap-Admin-Template/src/assets"));
 app.listen(3000);
-
-// file uploader using multer
-// Step 1. npm install multer
-// step 2. instance obejct for mutler.
-// step 3. monitor post method using app.post, running uploader front to backend if a user requests to you web.
-var multer = require('multer');
-
-//app.post('/upload_form', upload.single('imageFile'), function(req, res){
-//	console.log("test1");
-//});
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-  	console.log(req);
-    cb(null, 'uploaded/'); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
-  }
-  filename: function (req, file, cb) {
-  	console.log(req);
-    cb(null, file.originalname); // cb 콜백함수를 통해 전송된 파일 이름 설정
-  }
-})
-var upload = multer({ storage: storage })
-app.post('/upload_form', upload.single('imageFile'), function(req, res){
-	console.log("test1");
-});
