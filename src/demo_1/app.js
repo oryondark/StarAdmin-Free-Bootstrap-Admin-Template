cb985601d07c864208f9040010d83acf2ebf8e42
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-10-16 15:00:16 +0900

    update

--- src/demo_1/app.js
+++ src/demo_1/app.js
@@ -5,8 +5,6 @@ var app = express();
 var path = require('path');
 const saveFolder = 'uploaded/'
 
-// viewed at http://localhost:8080
-
 app.get('/', function(req, res) {
     res.sendFile(path.join("/home/ubuntu/StarAdmin-Free-Bootstrap-Admin-Template/src/demo_1" + '/index.html'));
 });
