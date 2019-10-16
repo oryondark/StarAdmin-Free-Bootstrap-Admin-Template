522a57087ccda4ce132c7744d97b678d5a89cbc6
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-10-16 15:10:59 +0900

    update

--- src/demo_1/app.js
+++ src/demo_1/app.js
@@ -47,7 +47,7 @@ app.post('/upload_form', upload.single('fileUpload'), (req, res) => {
 	const cmd = spawn('/usr/bin/python3', ['../assets/knn_module/inference.py', saveFolder+fileName])
 	cmd.stdout.on('data', (data) => {
 		console.log("success")
-		return data;
+		return res.json();
 	});
 	cmd.stderr.on('data', (data) => {
 		console.log(`err : ${data}`);
