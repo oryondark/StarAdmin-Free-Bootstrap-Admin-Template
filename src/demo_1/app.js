57e9f6416177923e3c1f0c030556e4953a8993c6
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-10-16 12:36:34 +0900

    update

--- src/demo_1/app.js
+++ src/demo_1/app.js
@@ -37,7 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage});
 app.post('/upload_form', upload.single('fileUpload'), (req, res) => {
 	//res.send('success');
-	console.log(res.file);
+	console.log(req.file);
 	const { spawn } = require('child_process');
 	//const cmd = spawn('/usr/bin/python3', ['./test.py'])
 	//cmd.stdout.on('data', (data) =>{
